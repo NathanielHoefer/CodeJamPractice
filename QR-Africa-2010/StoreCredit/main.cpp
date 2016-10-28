0d82ec4ab156d84d6ea1f1f872ac7ace9df4e640
Nathaniel Hoefer <nathaniel.d.hoefer@gmail.com>
2016-10-27 20:50:17 -0500

    QR-Africa-2010_StoreCredit: Completed

--- QR-Africa-2010/StoreCredit/main.cpp
+++ QR-Africa-2010/StoreCredit/main.cpp
@@ -4,60 +4,116 @@
  *  Created on: Oct 24, 2016
  *      Author: Nathaniel
  */
-
+#include <cstring>
+#include <string.h>
+#include <map>
+#include <deque>
+#include <queue>
+#include <stack>
+#include <sstream>
 #include <iostream>
+#include <iomanip>
+#include <cstdio>
+#include <cmath>
+#include <cstdlib>
+#include <ctime>
+#include <algorithm>
+#include <vector>
+#include <set>
+#include <complex>
+#include <list>
 
 using namespace std;
 
+#define pb push_back
+#define all(v) v.begin(),v.end()
+#define rall(v) v.rbegin(),v.rend()
+#define sz size()
+#define rep(i,m) for(int i=0;i<(int)(m);i++)
+#define rep2(i,n,m) for(int i=n;i<(int)(m);i++)
+#define For(it,c) for(__typeof(c.begin()) it=c.begin();it!=c.end();++it)
+#define mem(a,b) memset(a,b,sizeof(a))
+#define mp make_pair
+#define dot(a,b) ((conj(a)*(b)).X)
+#define X real()
+#define Y imag()
+#define length(V) (hypot((V).X,(V).Y))
+#define vect(a,b) ((b)-(a))
+#define cross(a,b) ((conj(a)*(b)).imag())
+#define normalize(v) ((v)/length(v))
+#define rotate(p,about,theta) ((p-about)*exp(point(0,theta))+about)
+#define pointEqu(a,b) (comp(a.X,b.X)==0 && comp(a.Y,b.Y)==0)
 
+typedef stringstream ss;
+typedef pair<int, int> pii;
+typedef vector<pii> vpii;
+typedef vector<string> vs;
+typedef vector<int> vi;
+typedef vector<double> vd;
+typedef vector<vector<int> > vii;
+typedef long long ll;
+typedef long double ld;
+typedef complex<double> point;
+typedef pair<point, point> segment;
+typedef pair<double, point> circle;
+typedef vector<point> polygon;
+
+const int oo = (int) 1e9;
+const double PI = 2 * acos(0);
+const double eps = 1e-9;
+
+#define SMALL
+#define LARGE
 int main() {
+	freopen("a.txt", "rt", stdin);
+#ifdef SMALL
+	freopen("A-small-practice.in","rt",stdin);
+	freopen("A-small.out","wt",stdout);
+#endif
+#ifdef LARGE
+	freopen("A-large-practice.in","rt",stdin);
+	freopen("A-large.out","wt",stdout);
+#endif
 
 	int numOfCases, credit, items;
 	int x, y;
 
 	cin >> numOfCases; // Read in the number of test cases
 
-	cout << numOfCases << endl;
-
 	for ( int i = 1; i <= numOfCases; i++ ) {
 
 		cin >> credit;
 		cin >> items;
-
-		cout << credit << " " << items << endl;
 
 		int list[items];
 
 		// Reads the item prices into list array
 		for ( int j = 0; j < items; j++ ) {
 			cin >> list[j];
-			cout << list[j] << " ";
 		}
 
 
 		bool valFound = false;
+		int sum;
 
 		// Cycles through each combination until the sum of two equals the
 		// credit amount
 		for ( x = 0; x < items; x++ ) {
 			for ( y = x + 1; y < items; y++ ) {
-
-				cout << "Cmp " << list[x] << " " << list[y] << endl;
-				cout << (list[x] + list[y]) << endl;
-
-				if ( (list[x] + list[y]) == credit ) {
+				sum = list[x] + list[y];
+				if ( sum == credit ) {
 					valFound = true;
 					break;
 				}
 			}
 
-			if ( valFound )
-				cout << "val found" << endl;
+			if ( valFound ) {
 				valFound = false;
 				break;
+			}
 		}
 
-		cout << "Case #" << i << ": " << x + 1 << " " << y + 1 << endl << endl;
+		cout << "Case #" << i << ": " << x + 1 << " " << y + 1 << endl;
 	}
 
 	return 0;
